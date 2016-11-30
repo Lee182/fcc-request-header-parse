450d89cf6f7fc746fe9e94397b8534181ab64755
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-11-30 09:46:58 +0000

    commit with parser

---
+++ README.md
@@ -0,0 +1,11 @@
+# fcc-request-header-parser
+heroku link [http://link.com](http://link.com/)
+
+Simple server that returns the extracts the following information for any GET request.
+```javascript
+{
+  "ipaddress":"::1",
+  "language":"en-gb",
+  "software":"Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko"
+}
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const url = require('url')
+let port = 3000
+
+function extractStrfromBrachets(str) {
+  var b = str.match(/\(.*\)/)
+  if (b === null) {return str}
+  arr = b[0].split('')
+  arr[0] = ''
+  arr[arr.length-1] = ''
+  return arr.join('')
+}
+
+const app = http.createServer(function(req, res){
+  if (req.method === 'GET') {
+    var str = url.parse(req.url).pathname === 'whoami'
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    let obj = {
+      ipaddress: req.connection.remoteAddress,
+      language: req.headers['accept-language'].split(',')[0],
+      software: extractStrfromBrachets( req.headers['user-agent'] )
+    }
+    res.write( JSON.stringify(obj) )
+    res.end()
+  }
+  res.end()
+  return
+})
+app.listen(port, function(){
+  console.log('server listening at http://localhost:'+port+'/')
+})
